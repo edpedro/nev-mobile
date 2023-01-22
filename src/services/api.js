ec5d263cfa398148e2a3d876fc37d83b8a8ce752
Eduardo <edp2013.ep@gmail.com>
2023-01-22 20:43:51 -0300

    update 2023

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.112:3333'
+  baseURL: "http://192.168.0.106:3333",
   //baseURL: "https://api-server-ts.herokuapp.com"
-})
-export default api
+});
+export default api;
